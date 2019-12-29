9b2a7fb4b725e04996ae7afb9d2192b35a1bc565
Mahir Korac <koracdevelopers@gmail.com>
2019-12-29 14:11:17 +0100

    :wrench: Making changes in model

--- app/src/main/java/com/mahir/flowrspottestproject/model/Flower.kt
+++ app/src/main/java/com/mahir/flowrspottestproject/model/Flower.kt
@@ -1,8 +1,8 @@
 package com.mahir.flowrspottestproject.model
 
 data class Flower(
-    val favorite: Boolean,
-    val id: Int,
+    val favorite: String,
+    val id: String,
     val latin_name: String,
     val name: String,
     val profile_picture: String,
