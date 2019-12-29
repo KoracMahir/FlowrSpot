d424b39c659ed95187f1070b0c49ccdd93314d09
Mahir Korac <koracdevelopers@gmail.com>
2019-12-29 14:09:58 +0100

    :lipstick: Updating the UI

--- app/src/main/java/com/mahir/flowrspottestproject/MainActivity.kt
+++ app/src/main/java/com/mahir/flowrspottestproject/MainActivity.kt
@@ -2,26 +2,29 @@ package com.mahir.flowrspottestproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Debug
 import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mahir.flowrspottestproject.adapter.CustomAdapter
 import com.mahir.flowrspottestproject.interfacex.IFlowerView
 import com.mahir.flowrspottestproject.model.Flower
 import com.mahir.flowrspottestproject.presenter.FlowerPresenter
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_home.*
 
 class MainActivity : AppCompatActivity(),IFlowerView{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView.layoutManager = GridLayoutManager(this,2)
+
+
+        FlowerPresenter(this).getDataFromApi()
     }
     override fun getFlowers(flower: List<Flower>) {
-        text1.text = flower[0].favorite.toString()
-        text2.text = flower[0].id.toString()
-        text3.text = flower[0].latin_name.toString()
-        text4.text = flower[0].name.toString()
-        text5.text = flower[0].profile_picture.toString()
-        text6.text = flower[0].sightings.toString()
+        val adapter = CustomAdapter(flower)
+        recyclerView.adapter = adapter
     }
 
     override fun onDataFailiure(throwable: Throwable) {
