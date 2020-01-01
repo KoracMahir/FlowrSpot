43eecc6b44fadb88c0f22f2bab077c109edaa2d4
Mahir Korac <koracdevelopers@gmail.com>
2020-01-01 12:24:33 +0100

    search logic

--- app/src/main/java/com/mahir/flowrspottestproject/MainActivity.kt
+++ app/src/main/java/com/mahir/flowrspottestproject/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.mahir.flowrspottestproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.os.Debug
 import android.text.Editable
-import android.text.TextWatcher
-import android.widget.EditText
-import android.widget.SearchView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.mahir.flowrspottestproject.adapter.CustomAdapter
 import com.mahir.flowrspottestproject.interfacex.IFlowerView
 import com.mahir.flowrspottestproject.model.Flower
 import com.mahir.flowrspottestproject.presenter.FlowerPresenter
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.Locale.filter
+import android.text.TextWatcher;
+import android.view.View
 
 class MainActivity : AppCompatActivity(),IFlowerView{
 
@@ -23,9 +21,25 @@ class MainActivity : AppCompatActivity(),IFlowerView{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        FlowerPresenter(this).getSeachDataFromApi("Alpski volcin")
+        FlowerPresenter(this).getDataFromApi(0)
+        GetSeachableText(this)
 
-
+    }
+    fun GetSeachableText(context: Context){
+        menu_search.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if(p0.toString()=="")
+                    FlowerPresenter(context).getDataFromApi(0)
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                FlowerPresenter(context).getSeachDataFromApi(p0.toString())
+            }
+        })
+    }
+    fun ParseSeachableText(seach:String):String{
+        return seach
     }
     override fun getFlowers(flower: List<Flower>) {
         recyclerView.layoutManager = GridLayoutManager(this,2)
