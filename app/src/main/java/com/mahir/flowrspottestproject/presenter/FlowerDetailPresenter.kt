19cadaa327c4278f5efaf2874745e9abf9950872
Mahir Korac <koracdevelopers@gmail.com>
2020-01-10 12:18:12 +0100

    Adding favorite button

--- app/src/main/java/com/mahir/flowrspottestproject/presenter/FlowerDetailPresenter.kt
+++ app/src/main/java/com/mahir/flowrspottestproject/presenter/FlowerDetailPresenter.kt
@@ -1,16 +1,11 @@
 package com.mahir.flowrspottestproject.presenter
 
-import android.widget.Toast
 import com.mahir.flowrspottestproject.interfacex.FlowerDetailView
-import com.mahir.flowrspottestproject.model.Flower
-import com.mahir.flowrspottestproject.model.FlowerDetail
-import com.mahir.flowrspottestproject.model.FlowerX
-import com.mahir.flowrspottestproject.model.Flowers
+import com.mahir.flowrspottestproject.model.*
 import com.mahir.flowrspottestproject.services.RetrofitServices
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import kotlin.coroutines.coroutineContext
 
 class FlowerDetailPresenter(flowerDetailView: FlowerDetailView){
     val flowerDetailView = flowerDetailView
@@ -29,6 +24,20 @@ class FlowerDetailPresenter(flowerDetailView: FlowerDetailView){
                     flowerDetailView.moveProgressBar()
                 }
 
+            })
+    }
+    fun setFlowerFavorite(flower_id:Int,auth_key:String?){
+        RetrofitServices.create()
+            .postLike(flower_id,auth_key)
+            .enqueue(object:Callback<DeleteFavorite>{
+
+                override fun onResponse(call: Call<DeleteFavorite>,response: Response<DeleteFavorite>) {
+                    flowerDetailView.getflowerfavorite(response.body()!!.user_id)
+                }
+
+                override fun onFailure(call: Call<DeleteFavorite>, t: Throwable) {
+
+                }
             })
     }
 }
--- app/src/main/java/com/mahir/flowrspottestproject/presenter/FlowerPresenter.kt
+++ app/src/main/java/com/mahir/flowrspottestproject/presenter/FlowerPresenter.kt
@@ -1,9 +1,12 @@
 package com.mahir.flowrspottestproject.presenter
 
-import android.content.Context
 import com.mahir.flowrspottestproject.interfacex.IFlowerView
+import com.mahir.flowrspottestproject.model.DeleteFavorite
+import com.mahir.flowrspottestproject.model.FavoriteFlower.FavFlower
+import com.mahir.flowrspottestproject.model.FavoriteFlower.FavoriteFlowers
 import com.mahir.flowrspottestproject.model.Flower
 import com.mahir.flowrspottestproject.model.Flowers
+import com.mahir.flowrspottestproject.model.LoginResponse
 import com.mahir.flowrspottestproject.services.RetrofitServices
 import retrofit2.Call
 import retrofit2.Callback
@@ -13,7 +16,7 @@ class FlowerPresenter(flowerView: IFlowerView){
     val flowerView = flowerView
 
     fun getDataFromApi(page:Int){
-//        flowerView.showProgressBar()
+        flowerView.showProgressBar()
         RetrofitServices.create()
             .getApi(page)
             .enqueue(object : Callback<Flowers>{
@@ -28,7 +31,7 @@ class FlowerPresenter(flowerView: IFlowerView){
             })
     }
     fun getSeachDataFromApi(flowername:String){
-//        flowerView.showProgressBar()
+        flowerView.showProgressBar()
         RetrofitServices.create()
             .getSearchApi(flowername)
             .enqueue(object : Callback<Flowers>{
@@ -43,4 +46,49 @@ class FlowerPresenter(flowerView: IFlowerView){
             })
     }
 
+    fun getFavorite(page:Int,auth_key:String?){
+        RetrofitServices.create()
+            .getFavorite(page,auth_key)
+            .enqueue(object : Callback<FavoriteFlowers>{
+                override fun onResponse(call: Call<FavoriteFlowers>, response: Response<FavoriteFlowers>) {
+                    if (response.code()==401)
+                        flowerView.refreshTokenFailed()
+                    else
+                        flowerView.getFavorites(response.body()?.fav_flowers as List<FavFlower>)
+                }
+
+                override fun onFailure(call: Call<FavoriteFlowers>, t: Throwable) {
+                    flowerView.onDataFailiure(t)
+                }
+            })
+    }
+    fun setFlowerFavorite(flower_id:Int,auth_key:String?){
+        RetrofitServices.create()
+            .postLike(flower_id,auth_key)
+            .enqueue(object: Callback<DeleteFavorite> {
+                override fun onResponse(call: Call<DeleteFavorite>, response: Response<DeleteFavorite>) {
+
+                }
+
+                override fun onFailure(call: Call<DeleteFavorite>, t: Throwable) {
+
+                }
+            })
+    }
+
+    fun refreshToken(auth_key:String){
+        RetrofitServices.create()
+            .refreshToken(auth_key)
+            .enqueue(object : Callback<LoginResponse> {
+
+                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                    flowerView.refreshToken(response.body()?.auth_token.toString())
+                }
+
+                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                    flowerView.refreshTokenFailed()
+                }
+
+            })
+    }
 }
