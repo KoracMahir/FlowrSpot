8cdc0f212af288b9441f269c879ddf95332476e8
Mahir Korac <koracdevelopers@gmail.com>
2020-01-20 20:16:49 +0100

    Adding Favorite Flowers Screen

---
+++ app/src/main/java/com/mahir/flowrspottestproject/adapter/favorite_adapter/FavoriteAdapter.kt
@@ -0,0 +1,30 @@
+package com.mahir.flowrspottestproject.adapter.favorite_adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mahir.flowrspottestproject.R
+import com.mahir.flowrspottestproject.adapter.favorite_adapter.FavoriteViewHolder
+import com.mahir.flowrspottestproject.model.Flower
+
+class FavoriteAdapter : RecyclerView.Adapter<FavoriteViewHolder>(){
+
+    var flowerList: List<Flower> = emptyList()
+
+    fun addItems(flist:List<Flower>){
+        this.flowerList = flist
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false)
+        return FavoriteViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return flowerList.size
+    }
+
+    override fun onBindViewHolder(holder: FavoriteViewHolder, position: Int) {
+        holder.bindItems(flowerList[position])
+    }
+}
---
+++ app/src/main/java/com/mahir/flowrspottestproject/adapter/favorite_adapter/FavoriteViewHolder.kt
@@ -0,0 +1,12 @@
+package com.mahir.flowrspottestproject.adapter.favorite_adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.mahir.flowrspottestproject.model.Flower
+
+class FavoriteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    fun bindItems(flower: Flower){
+
+    }
+}
