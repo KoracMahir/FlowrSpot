cbb7d9a0986fd5113672920cf986546f2ca012b8
Mahir Korac <koracdevelopers@gmail.com>
2020-01-21 14:19:24 +0100

    Updating FavoriteFlowers Screen

---
+++ app/src/main/java/com/mahir/flowrspottestproject/presenter/FavFlowerPresenter.kt
@@ -0,0 +1,31 @@
+package com.mahir.flowrspottestproject.presenter
+
+import com.mahir.flowrspottestproject.interfacex.FavFlowersView
+import com.mahir.flowrspottestproject.model.FavoriteFlower.FavoriteFlowersResponese
+import com.mahir.flowrspottestproject.model.FlowersResponese
+import com.mahir.flowrspottestproject.services.RetrofitServices
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class FavFlowerPresenter(favFlowersView: FavFlowersView) {
+
+    val favFlowersView = favFlowersView
+
+    fun getFavorite(page:Int,auth_key:String?){
+        RetrofitServices.create()
+            .getFavorite(page,auth_key)
+            .enqueue(object : Callback<FlowersResponese> {
+                override fun onResponse(call: Call<FlowersResponese>, response: Response<FlowersResponese>) {
+                    if (response.code()==401)
+                        favFlowersView.onTokenFailiure()
+                    else
+                        favFlowersView.getFavorites(response.body()?.fav_flowers as List<FavoriteFlowersResponese>)
+                }
+
+                override fun onFailure(call: Call<FlowersResponese>, t: Throwable) {
+
+                }
+            })
+    }
+}
