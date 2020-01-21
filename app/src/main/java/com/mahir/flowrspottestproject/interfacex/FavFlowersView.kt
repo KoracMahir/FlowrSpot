b86d0b5f27cbbdc77b1aad5ab875643be99f5e85
Mahir Korac <koracdevelopers@gmail.com>
2020-01-21 14:17:19 +0100

    Updating FavoriteFlowers Screen

---
+++ app/src/main/java/com/mahir/flowrspottestproject/interfacex/FavFlowersView.kt
@@ -0,0 +1,8 @@
+package com.mahir.flowrspottestproject.interfacex
+
+import com.mahir.flowrspottestproject.model.FavoriteFlower.FavoriteFlowersResponese
+
+interface FavFlowersView{
+    fun getFavorites(flowers: List<FavoriteFlowersResponese>)
+    fun onTokenFailiure()
+}
--- app/src/main/java/com/mahir/flowrspottestproject/views/FavoriteFlowersFragment.kt
+++ app/src/main/java/com/mahir/flowrspottestproject/views/FavoriteFlowersFragment.kt
@@ -2,25 +2,50 @@ package com.mahir.flowrspottestproject.views
 
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.mahir.flowrspottestproject.R
+import com.mahir.flowrspottestproject.adapter.favorite_adapter.FavoriteAdapter
+import com.mahir.flowrspottestproject.interfacex.FavFlowersView
+import com.mahir.flowrspottestproject.model.FavoriteFlower.FavoriteFlowersResponese
+import com.mahir.flowrspottestproject.presenter.FavFlowerPresenter
+import kotlinx.android.synthetic.main.fragment_favorite_flowers.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class FavoriteFlowersFragment : Fragment() {
+class FavoriteFlowersFragment : Fragment(),FavFlowersView {
 
+
+    var adapter = FavoriteAdapter()
+    var auth_key : String = "nista"
+    val favFlowerPresenter = FavFlowerPresenter(this)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_favorite_flowers, container, false)
     }
 
-
+    override fun onStart() {
+        super.onStart()
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        prefs.apply {
+            auth_key = getString("auth_token","")
+        }
+        favFlowerPresenter.getFavorite(1,auth_key)
+    }
+    override fun getFavorites(flowers: List<FavoriteFlowersResponese>) {
+        adapter.addItems(flowers)
+        recyclerView.adapter = adapter
+    }
+    override fun onTokenFailiure() {
+        findNavController().navigate(R.id.action_favoriteFlowersFragment_to_homeFragment)
+    }
 }
