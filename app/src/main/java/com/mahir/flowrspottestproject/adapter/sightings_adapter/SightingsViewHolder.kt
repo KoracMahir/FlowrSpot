6ba449160d9639074324b91e00bf4b0abe58b296
Mahir Korac <koracdevelopers@gmail.com>
2020-01-21 15:24:49 +0100

    Updating flower sightings list

--- app/src/main/java/com/mahir/flowrspottestproject/adapter/sightings_adapter/SightingsViewHolder.kt
+++ app/src/main/java/com/mahir/flowrspottestproject/adapter/sightings_adapter/SightingsViewHolder.kt
@@ -2,14 +2,27 @@ package com.mahir.flowrspottestproject.adapter.sightings_adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.mahir.flowrspottestproject.model.sightingmodels.FlowerSightingsModel
 import com.mahir.flowrspottestproject.model.sightingmodels.Sighting
 import kotlinx.android.synthetic.main.recycler_view_sighting.view.*
+import kotlinx.android.synthetic.main.recycler_view_sighting.view.name
+import kotlinx.android.synthetic.main.recycler_view_sighting.view.profile_picture
 
 class SightingsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
     fun bindItems(sightingList: Sighting){
+        val flowerimageURL : String
         itemView.name.text = sightingList.name
-        itemView.ownername.text = sightingList.user.full_name
+        itemView.ownername.text = "by "+sightingList.user.full_name
+        itemView.description.text = sightingList.description
+        itemView.commentnum.text = sightingList.comments_count.toString()+" Comments"
+        itemView.fav_num.text = sightingList.likes_count.toString()+" Favorites"
+        flowerimageURL = sightingList.picture
+        Glide.with(itemView)
+            .load("https:"+flowerimageURL)
+            .apply(RequestOptions.circleCropTransform())
+            .into(itemView.profile_picture)
     }
 }
